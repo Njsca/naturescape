99b01d07cb5a1a90fbbb9b533426c119b611fe57
Anja Ottiger <anjakyraottiger@gmail.com>
2022-08-30 17:30:15 +0200

    cards update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :uikit ]
 
   def home
+  end
+
+  def uikit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/uikit", to: "pages#uikit"
+  resources :hikes
   # Defines the root path route ("/")
   # root "articles#index"
 end
