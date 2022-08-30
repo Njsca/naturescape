651be17aea1e411748c553c947cc1eb6a738d75c
Denis Cantor <deniscantor7@gmail.com>
2022-08-30 13:05:42 +0200

    added coordinates to hikes

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,7 @@ class Hike < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :title, :date, :location, :description, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
