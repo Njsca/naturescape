ef251f13f284928b6d950295682a552263e74bbd
Joeri Verdegaal <joeriverdegaal@gmail.com>
2022-09-02 15:17:18 +0200

    new booking and chatroom page styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_01_134554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_134554) do
     t.datetime "updated_at", null: false
     t.index ["hike_id"], name: "index_bookings_on_hike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "chat_views", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "chatrooms", force: :cascade do |t|
