2ce1ad8593b48dee7c36a708fe0a07955268f370
Joeri Verdegaal <joeriverdegaal@gmail.com>
2022-09-09 00:26:52 +0200

    fixed chatroom problem and some little bugs around user journey

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :index, :uikit, :profile ]
+  skip_before_action :authenticate_user!, only: [ :home, :index, :uikit]
 
   def home
     @hike1 = Hike.first
